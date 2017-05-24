602da690d3503efcc52f207208442dc77113e32f
fcraft <fcraft@southern-tech.com>
2017-05-24 12:14:37 -0400

    Commit changes through first of the this code-along section

--- app/index.js
+++ app/index.js
@@ -1,19 +1,8 @@
  var React = require('react');
  var ReactDOM = require('react-dom');
  require('./index.css');
-
-// state
-// lifecicle events
-// UI
-
- class APP extends React.Component {
-   render () {
-     return (
-       <div> Hello Worldlings! </div>
-     )
-   }
- }
+ var App = require("./components/App");
 
  ReactDOM.render (
-   <APP />, document.getElementById('app')
+   <App />, document.getElementById('app')
  );
