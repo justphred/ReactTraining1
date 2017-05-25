e4c78613cba7e9fe90e6ef58b48e32629a24d7d6
fcraft <fcraft@southern-tech.com>
2017-05-25 11:53:16 -0400

    In midst of implementing click event react style

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,15 +1,35 @@
 var React = require('react');
 
 class Popular extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    }
+    // Ensure that the context of the this keyword when invoked/used
+    // within the method updateLanguage is set to that of this class (or the object created from this class)
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState( function(){
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
   render () {
     var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
     return (
       <ul className="languages">
         {languages.map(function(lang) {
           return (
-            <li key={lang}>{lang}</li>
+            <li
+              onClick={this.updateLanguage.bind(null, lang)}
+              key={lang}>
+              {lang}
+            </li>
           )
-        })
+        }, this)
         }
       </ul>
     )
