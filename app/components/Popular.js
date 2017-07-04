843edb959bb875daa0b968c2eea2db5fa768ddbb
Fred Craft <splatmakle@gmail.com>
2017-07-04 13:53:47 -0400

    Midstream React Router lesson commit

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -27,6 +27,35 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired
 }
 
+function RepoGrid (props) {
+  return (
+    <ul className="popular-list">
+      {props.repos.map(function(repo, index){
+        return (
+        <li key={repo.name} className="popular-item">
+          <div className="popular-rank">#{index+1}</div>
+          <ul className="space-list-items">
+            <li>
+              <img
+                className="avatar"
+                src={repo.owner.avatar_url}
+                alt={"Avatar for " + repo.owner.login} />
+            </li>
+            <li> <a href={repo.html_url}>{repo.name}</a> </li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count} stars</li>
+          </ul>
+        </li>
+       )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 class Popular extends React.Component{
   constructor (props) {
     super(props);
@@ -50,23 +79,30 @@ class Popular extends React.Component{
     });
 
     api.fetchPopularRepos(lang)
-      .then(function(repos){
-        console.log(repos);
+      .then( function(repos) {
+        // console.log(repos);
         this.setState( function() {
           return {
             repos: repos
           }
-        }.bind(this) );
-    });
+        });
+    }.bind(this));
   }
   render () {
     return (
-      <SelectLanguage
-        selectedLanguage={this.state.selectedLanguage}
-        onSelect={this.updateLanguage}
-      />
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        {(!this.state.repos)
+          ? <p>LOADING</p>
+          : <RepoGrid repos={this.state.repos} /> }        
+      </div>
     )
   }
 }
 
 module.exports = Popular;
+
+// {JSON.stringify(this.state.repos, 2, null)}
