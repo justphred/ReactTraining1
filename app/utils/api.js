6dc082fd085a4a440a65c5c14876943f0b897523
fcraft <fcraft@southern-tech.com>
2017-08-04 12:02:55 -0400

    Initial mods for extended github api requests

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,9 +1,41 @@
 var axios = require('axios');
 
+// Github client id and "secret id" will be required if you hit then
+// the Github api interface to often within some period of time.
+// "Google" Github client id to figure out how to get one.
+// var github_id = "MY_CLIENT_ID";
+// var github_sec_id = "MY_SECRET_ID";
+// var params = "?client_id=" + github_id + "&client_secret" + github_sec_id;
+var params = "";
+
+function getProfile( username ) {
+  return axios.get("https://api.github.com/users/" + username + params)
+    .then( function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get("https://api.github.com/users/" + username + "/repos" + params + "&per_page=100");
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore (profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI =
-       window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');      
+       window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
     return axios.get(encodedURI)
       .then( function(response) {
         return response.data.items;
